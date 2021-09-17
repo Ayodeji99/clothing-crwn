fa8c9d0684bf6158200e2c26ae0b86bdd5d777d0
ayodeji99 <adedeji.aransiola@yahoo.com>
2021-09-17 02:05:10 +0100

    implemented firebase utils, included ability to store email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,37 @@ const config = {
     measurementId: "G-0GF5FYL4S7"
 };
 
+
+export const createUserProfileDocument = async ( userAuth, additionalData ) => { 
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    console.log(snapShot);
+
+    if(!snapShot.exists) {
+        const { email, displayName } = userAuth;
+        const createdAt = new Date();
+         try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+
+    return userRef;
+};
+
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
